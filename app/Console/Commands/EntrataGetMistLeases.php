1d90319b0916647efe7455797a6077f40b2cf61a
Mitul Gadhiya <gadhia.mitul41@gmail.com>
2020-11-26 00:56:30 +0530

    Entrata Branch

--- app/Console/Commands/EntrataGetMistLeases.php
+++ app/Console/Commands/EntrataGetMistLeases.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Apartment;
 use App\Entrata\Leases;
 use App\User;
 use Carbon\Carbon;
@@ -41,32 +40,28 @@ class EntrataGetMistLeases extends Command
      */
     public function handle()
     {
-        $apartments = Apartment::whereNotNull('unit_number')->get();
-        $bar = $this->output->createProgressBar(count($apartments));
+        $leases = Leases::getMitsLeases();
+        $bar = $this->output->createProgressBar(count($leases));
         $bar->start();
-        foreach ($apartments as $apartment) {
-            $unit_number = $apartment->unit_number;
-            $leases = Leases::getMitsLeases($unit_number);
-            foreach ($leases as $lease) {
-                $start_date = null;
-                $end_date = null;
-                $lease_id = $lease['Identification']['IDValue'];
-                foreach ($lease['LeaseEvents']['LeaseEvent'] as $leaseEvent) {
-                    if ($leaseEvent['@attributes']['EventType'] == 'LeaseFrom') {
-                        $start_date = $leaseEvent['@attributes']['Date'];
-                    } elseif ($leaseEvent['@attributes']['EventType'] == 'ActualMoveIn') {
-                        $start_date = $leaseEvent['@attributes']['Date'];
-                    } elseif ($leaseEvent['@attributes']['EventType'] == 'LeaseTo') {
-                        $end_date = $leaseEvent['@attributes']['Date'];
-                    } elseif ($leaseEvent['@attributes']['EventType'] == 'ActualMoveOut') {
-                        $end_date = $leaseEvent['@attributes']['Date'];
-                    }
-                }
-                \DB::table('users')
-                    ->where('lease_id', '=', $lease_id)
-                    ->update(['start_date' => $start_date, 'end_date' => $end_date]);
-            }
+        foreach ($leases as $lease) {
             $bar->advance();
+            $start_date = null;
+            $end_date = null;
+            $lease_id = $lease['Identification']['IDValue'];
+            foreach ($lease['LeaseEvents']['LeaseEvent'] as $leaseEvent) {
+                if ($leaseEvent['@attributes']['EventType'] == 'LeaseFrom') {
+                    $start_date = $leaseEvent['@attributes']['Date'];
+                } elseif ($leaseEvent['@attributes']['EventType'] == 'ActualMoveIn') {
+                    $start_date = $leaseEvent['@attributes']['Date'];
+                } elseif ($leaseEvent['@attributes']['EventType'] == 'LeaseTo') {
+                    $end_date = $leaseEvent['@attributes']['Date'];
+                } elseif ($leaseEvent['@attributes']['EventType'] == 'ActualMoveOut') {
+                    $end_date = $leaseEvent['@attributes']['Date'];
+                }
+            }
+            \DB::table('users')
+                ->where('lease_id', '=', $lease_id)
+                ->update(['start_date' => $start_date, 'end_date' => $end_date]);
         }
         $bar->finish();
 
@@ -76,7 +71,7 @@ class EntrataGetMistLeases extends Command
             ->where('end_date', '>=', Carbon::today()->toDateString())
             ->update(['is_enabled' => '1', 'updated_at' => Carbon::now()]);
 
-        // De-Activate Users
+        // De-Activate Usersz
         $deactivated_users = \DB::table('users')
             ->where('start_date', '>', Carbon::today()->toDateString())
             ->orWhere('end_date', '<', Carbon::today()->toDateString())
@@ -84,6 +79,7 @@ class EntrataGetMistLeases extends Command
             ->orWhereNull('end_date')
             ->update(['is_enabled' => '0', 'updated_at' => Carbon::now()]);
 
+        echo PHP_EOL;
         echo User::all()->count() . " Total Users as of " . Carbon::today()->toDateString() . PHP_EOL;
         echo "$activated_users Users Activated as of " . Carbon::today()->toDateString() . PHP_EOL;
         echo "$deactivated_users Users De-Activated " . Carbon::today()->toDateString() . PHP_EOL;
