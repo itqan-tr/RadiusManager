676a1a2348341c2d6b506c1a6ea92b086fcfc758
Mitul Gadhia <mitul@gozonewifi.com>
2021-07-22 02:00:19 +0530

    Email template fixed.

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -10,13 +10,13 @@ IQ Machines welcomes you to {{ config('app.property_name') }}.  Your Username an
 
 Some devices such as TVs, game consoles, streaming devices, printers, and digital assistants will require you to
 add the device’s unique wireless Media Access Control (MAC) address to the network before the device will connect
-to the “{{ config('app.property_name') }}” network.<br>
+to the {{ config('app.property_name') }} network.<br>
 <br>
 Using our website below, you will be able to add your own devices to {{ config('app.property_name') }} network.
 Please note this process is only required for devices which will not connect to the WiFi using your regular username
 and password.<br>
 <br>
-Visit <a href="{{ config('app.url') }}">{{ config('app.url') }}</a> and sign in with your username and password.  After inputting the wireless MAC address of
+Visit <a target="_blank" href="{{ config('app.url') }}">{{ config('app.url') }}</a> and sign in with your username and password.  After inputting the wireless MAC address of
 your device to the website please connect that device to the WiFi titled {{ config('app.property_name') }} PSK and use the
 word {{ config('app.psk_password') }} as the password.<br>
 <br>
